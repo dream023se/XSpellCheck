387a38ed573dd0ca3f331c0bdfe7cb120e87c47c
dream023se <dubeysanchika82@gmail.com>
2024-05-10 14:43:24 +0530

    correct

---
+++ spellcheck/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Check from './component/Check/Check'
+function App() {
+  return (
+    <>
+    <Check/>
+    </>
+  );
+}
+
+export default App;
